2bb5bf18051f30a38b4b3ce27e35f53ab0373050
Alessio Buonomo <alessio@Alessio.PC>
2023-10-20 20:08:51 +0200

    Aggiunto il pulsante

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatChipsModule} from '@angular/material/chips';
 
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -63,6 +64,7 @@ register();
 	],
 	imports: [
 		MatExpansionModule,
+		MatChipsModule,
 		EventInfoComponent,
 		BrowserModule,
 		AppRoutingModule,
