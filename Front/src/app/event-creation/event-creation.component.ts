461494617aae2bce894a81b241510fe6539e2bc4
Davide <davide.cologgi96@gmail.com>
2023-10-21 18:07:09 +0200

    skin home + event-creation

--- Front/src/app/event-creation/event-creation.component.ts
+++ Front/src/app/event-creation/event-creation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AxiosService } from '../axios.service';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-event-creation',
@@ -9,43 +10,112 @@ import { AxiosService } from '../axios.service';
 export class EventCreationComponent {
   newEvent: any = {};
   imageFiles: File[] = []; // To store selected image files
+  fieldFocused: boolean = false;
+  fieldTouched: boolean = false;
+  fieldFocused2: boolean = false;
+  fieldTouched2: boolean = false;
+  fieldFocused3: boolean = false;
+  fieldTouched3: boolean = false;
+  eventForm: FormGroup;
+  commaPattern = /,/;
 
-  constructor(private axiosService: AxiosService) {}
+constructor(private axiosService: AxiosService, private formBuilder: FormBuilder) {
+    this.eventForm = this.formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required
+        ]
+      ],
+      description: [
+        '',
+        [
 
-  onSubmit(): void {
-    const userId = window.localStorage.getItem("userId");
-    const title = this.newEvent.name;
-    const description = this.newEvent.description;
-    const dateTimeLocal = this.newEvent.dateTime;
-    const place = this.newEvent.location;
-    const category = this.newEvent.category;
-
-    if (dateTimeLocal) {
-      const date = new Date(dateTimeLocal + 'Z');
-      const formattedDateTime = date.toISOString().substring(0, 23);
-      const endpoint = `/api/create-event/${userId}`;
-      const formData = new FormData();
-
-      for (const file of this.imageFiles) {
-        formData.append('photos', file, file.name);
-      }
-
-      formData.append('title', title);
-      formData.append('description', description);
-      formData.append('dateTime', formattedDateTime);
-      formData.append('place', place);
-      formData.append('category', category);
-
-      this.axiosService.requestMultipart(
-        "POST",
-        endpoint,
-        formData,
-      );
-      console.log('Form submitted!');
-    } else {
-      console.error('Invalid date and time.');
-    }
+        ]
+      ],
+      place: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.commaPattern),
+          Validators.pattern(/.*\d.*/)
+        ]
+      ],
+      dateTime: [
+        '',
+        [
+          Validators.required,
+          this.DateRangeValidator()
+        ]
+      ],
+      photo: [
+        '',
+        [
+          Validators.required
+        ]
+      ],
+      category: [
+        '',
+        [
+          Validators.required
+        ]
+      ]
+    })
   }
+
+DateRangeValidator(): Validators {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      if (control.value) {
+        const selectedDate = new Date(control.value);
+        const currentDate = new Date();
+        const maxDate = new Date();
+        maxDate.setFullYear(maxDate.getFullYear() + 1);
+
+        if (selectedDate < currentDate || selectedDate > maxDate) {
+          return { dateRange: true };
+        }
+      }
+      return null;
+    };
+}
+
+onSubmit(): void {
+
+	if (this.eventForm.valid) {
+		const userId = window.localStorage.getItem("userId");
+		const title = this.newEvent.name;
+		const description = this.newEvent.description;
+		const dateTimeLocal = this.newEvent.dateTime;
+		const place = this.newEvent.location;
+		const category = this.newEvent.category;
+
+		if (dateTimeLocal) {
+		const date = new Date(dateTimeLocal + 'Z');
+		const formattedDateTime = date.toISOString().substring(0, 23);
+		const endpoint = `/api/create-event/${userId}`;
+		const formData = new FormData();
+
+		for (const file of this.imageFiles) {
+			formData.append('photos', file, file.name);
+		}
+
+		formData.append('title', title);
+		formData.append('description', description);
+		formData.append('dateTime', formattedDateTime);
+		formData.append('place', place);
+		formData.append('category', category);
+
+		this.axiosService.requestMultipart(
+			"POST",
+			endpoint,
+			formData,
+			);
+			console.log('Form submitted!');
+		} else {
+			console.error('Invalid date and time.');
+		}
+	}
+}
 
   onFileChange(event: any): void {
     this.imageFiles = event.target.files;
