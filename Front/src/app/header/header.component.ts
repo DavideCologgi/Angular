09b4cd233796675fe34cf182f6a6ebd9eea6db80
Alessio Buonomo <abuonomo@e4r8p2.42roma.it>
2023-10-23 01:07:51 +0200

    Aggiunta logica per la gestione del title nell'header

--- Front/src/app/header/header.component.ts
+++ Front/src/app/header/header.component.ts
@@ -2,31 +2,46 @@ import { Component, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AxiosService } from '../axios.service';
 import { RedirectService } from '../redirect.service';
+import { HostListener } from '@angular/core';
+
 @Injectable()
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
   redirection = '';
   isLoggedIn: boolean = false;
   numeroNotifiche: any;
-  constructor(private router: Router, private axiosService: AxiosService,private redirectService: RedirectService) {
-	this.numeroNotifiche = 5;
+  imageName: string = '/assets/title.png'; // Imposta l'immagine predefinita
+  isMobile: boolean = false;
+
+  constructor(
+    private router: Router,
+    private axiosService: AxiosService,
+    private redirectService: RedirectService
+  ) {
+    this.numeroNotifiche = 5;
+	this.isMobile = window.innerWidth < 768;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isMobile = window.innerWidth < 768;
   }
 
   logout() {
     this.axiosService
-      .request2("POST", "api/auth/logout", {})
+      .request2('POST', 'api/auth/logout', {})
       .then((response) => {
-        this.isLoggedIn = false
+        this.isLoggedIn = false;
         //not logged
-        this.router.navigate(['/login'])
+        this.router.navigate(['/login']);
       })
       .catch((error) => {
-        console.error("Error logging out:", error);
+        console.error('Error logging out:', error);
       });
   }
 
@@ -39,39 +54,37 @@ export class HeaderComponent {
   }
 
   authenticate() {
-    this.axiosService.request(
-      "POST",
-      "api/authenticate",
-      {
-      }
-    ).then(response => {
-          console.log("test");
-            this.router.navigate([this.redirection]);
-				})
-			  .catch(error => {
-          console.log("error");
-        	this.router.navigate(['/login']);
-			  })
+    this.axiosService
+      .request('POST', 'api/authenticate', {})
+      .then((response) => {
+        console.log('test');
+        this.router.navigate([this.redirection]);
+      })
+      .catch((error) => {
+        console.log('error');
+        this.router.navigate(['/login']);
+      });
   }
 
   resetNumeroNotifiche() {
-	this.numeroNotifiche = 0;
+    this.numeroNotifiche = 0;
   }
 
-  navigateTo(route: string){
-	this.redirectService.setRedirect(route);
-	this.router.navigate([route]);
+  navigateTo(route: string) {
+    this.redirectService.setRedirect(route);
+    this.router.navigate([route]);
   }
 
-  navigateToMyEvents(){
-	this.redirectService.setRedirect("/my-events");
-	console.log(this.redirectService.getRedirect());
-	this.router.navigate(["/event-board"]);
+  navigateToMyEvents() {
+    this.redirectService.setRedirect('/my-events');
+    console.log(this.redirectService.getRedirect());
+    this.router.navigate(['/event-board']);
   }
 
-  navigateToRegisteredEvents(){
-	this.redirectService.setRedirect("/registered-events");
-	console.log(this.redirectService.getRedirect());
-	this.router.navigate(["/event-board"]);
+  navigateToRegisteredEvents() {
+    this.redirectService.setRedirect('/registered-events');
+    console.log(this.redirectService.getRedirect());
+    this.router.navigate(['/event-board']);
   }
+
 }
