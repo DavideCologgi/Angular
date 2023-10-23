2d826bde1c05da8c84e4cd7711175b863b3587a8
Alessio Buonomo <abuonomo@e4r8p2.42roma.it>
2023-10-23 04:23:18 +0200

    Aggiunti controlli sul tipo di file, aggiunto redirect a home quando fai logout

--- Front/src/app/header/header.component.ts
+++ Front/src/app/header/header.component.ts
@@ -38,7 +38,7 @@ export class HeaderComponent {
       .then((response) => {
         this.isLoggedIn = false;
         //not logged
-        this.router.navigate(['/login']);
+        this.router.navigate(['/home']);
       })
       .catch((error) => {
         console.error('Error logging out:', error);
